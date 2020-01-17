f300f35ff29a24779e184da24624f76b5c19ee54
Mattia Belletti <MBelletti@greenteam.it>
2020-01-17 12:13:27 +0100

    Logging test: shorter, functional.

--- src/logging.spec.js
+++ src/logging.spec.js
@@ -2,48 +2,38 @@ const { test } = require("./spec-helpers");
 
 const { getLoggingSystem } = require("./logging");
 
-const createLogger = () => {
+const testLogFunction = logFunction => (t, fn, expected) => {
   const entries = [];
-  const logFunction = str => entries.push(str);
-  const loggingSystem = getLoggingSystem(logFunction);
-  return { entries, ...loggingSystem };
+  const loggingSystem = getLoggingSystem(str => entries.push(str));
+  const data = { a: 1 };
+  t.equal(entries.length, 0);
+  t.equal(logFunction(loggingSystem, fn, data), data);
+  t.deepEqual(entries, [expected]);
+  t.end();
 };
 
-test("logging: logMonad can log a string", t => {
-  const { entries, logMonad } = createLogger();
-  const data = { a: 1 };
-  t.equal(entries.length, 0);
-  t.equal(logMonad("log entry")(data), data);
-  t.deepEqual(entries, ["log entry"]);
-  t.end();
-});
-
-test("logging: logMonad can log a string through a logging function", t => {
-  const { entries, logMonad } = createLogger();
-  const data = { a: 1 };
-  t.equal(entries.length, 0);
-  t.equal(logMonad(d => `log entry: ${d.a}`)(data), data);
-  t.deepEqual(entries, ["log entry: 1"]);
-  t.end();
-});
-
-test("logging: logContent can log a string", t => {
-  const { entries, logContent } = createLogger();
-  const data = { a: 1 };
-  t.equal(entries.length, 0);
-  t.equal(logContent(data, "log entry"), data);
-  t.deepEqual(entries, ["log entry"]);
-  t.end();
-});
-
-test("logging: logContent can log a string through a logging function", t => {
-  const { entries, logContent } = createLogger();
-  const data = { a: 1 };
-  t.equal(entries.length, 0);
-  t.equal(
-    logContent(data, d => `log entry: ${d.a}`),
-    data
-  );
-  t.deepEqual(entries, ["log entry: 1"]);
-  t.end();
-});
+[
+  {
+    fnName: "logMonad",
+    fnTest: testLogFunction(({ logMonad }, fn, data) => logMonad(fn)(data))
+  },
+  {
+    fnName: "logContent",
+    fnTest: testLogFunction(({ logContent }, fn, data) => logContent(data, fn))
+  }
+].forEach(({ fnName, fnTest }) =>
+  [
+    {
+      what: "a string",
+      value: "log entry",
+      expected: "log entry"
+    },
+    {
+      what: "a string through a logging function",
+      value: d => `log entry: ${d.a}`,
+      expected: "log entry: 1"
+    }
+  ].forEach(({ what, value, expected }) =>
+    test(`logging: ${fnName} can log ${what}`, t => fnTest(t, value, expected))
+  )
+);
